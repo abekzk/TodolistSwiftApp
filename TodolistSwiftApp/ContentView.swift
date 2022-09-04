58b021a91c0439292123aaf4620111a82041d637
kzuabe <ai3ekzk@gmail.com>
2022-09-04 23:36:49 +0900

    Initial Commit

---
+++ TodolistSwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodolistSwiftApp
+//
+//  Created by Kazuki Abe on 2022/09/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodolistSwiftApp/TodolistSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistSwiftAppApp.swift
+//  TodolistSwiftApp
+//
+//  Created by Kazuki Abe on 2022/09/04.
+//
+
+import SwiftUI
+
+@main
+struct TodolistSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
